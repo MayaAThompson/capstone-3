af7c54878b2451675bc41db0c338d9df341d9630
Maya Thompson <mayaandromedathompson@gmail.com>
2025-06-23 12:03:11 -0500

    added path variables and DAO method calls to the methods in CategoriesController. Added functionality to getAllCategories, getById, and create in MySqlCategoryDao.

--- capstone-starter/src/main/java/org/yearup/controllers/CategoriesController.java
+++ capstone-starter/src/main/java/org/yearup/controllers/CategoriesController.java
@@ -1,9 +1,8 @@
 package org.yearup.controllers;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.access.annotation.Secured;
+import org.springframework.web.bind.annotation.*;
 import org.yearup.data.CategoryDao;
 import org.yearup.data.ProductDao;
 import org.yearup.models.Category;
@@ -21,8 +20,12 @@ public class CategoriesController
     private CategoryDao categoryDao;
     private ProductDao productDao;
 
-
     // create an Autowired controller to inject the categoryDao and ProductDao
+    @Autowired
+    public CategoriesController(CategoryDao categoryDao, ProductDao productDao) {
+        this.categoryDao = categoryDao;
+        this.productDao = productDao;
+    }
 
     @GetMapping("/categories")
     public List<Category> getAll()
@@ -31,41 +34,50 @@ public class CategoriesController
     }
 
     // add the appropriate annotation for a get action
+    @GetMapping("/categories/{id}")
     public Category getById(@PathVariable int id)
     {
         // get the category by id
-        return null;
+        return categoryDao.getById(id);
     }
 
     // the url to return all products in category 1 would look like this
     // https://localhost:8080/categories/1/products
-    @GetMapping("{categoryId}/products")
+    @GetMapping("/categories/{categoryId}/products")
     public List<Product> getProductsById(@PathVariable int categoryId)
     {
         // get a list of product by categoryId
-        return null;
+        return productDao.listByCategoryId(categoryId);
     }
 
     // add annotation to call this method for a POST action
     // add annotation to ensure that only an ADMIN can call this function
+    @PostMapping("/categories")
+    @Secured("ROLE_ADMIN")
     public Category addCategory(@RequestBody Category category)
     {
         // insert the category
-        return null;
+        return categoryDao.create(category);
     }
 
     // add annotation to call this method for a PUT (update) action - the url path must include the categoryId
     // add annotation to ensure that only an ADMIN can call this function
+    @PutMapping("/categories/{id}")
+    @Secured("ROLE_ADMIN")
     public void updateCategory(@PathVariable int id, @RequestBody Category category)
     {
         // update the category by id
+        categoryDao.update(id, category);
     }
 
 
     // add annotation to call this method for a DELETE action - the url path must include the categoryId
     // add annotation to ensure that only an ADMIN can call this function
+    @DeleteMapping("/categories/{id}")
+    @Secured("ROLE_ADMIN")
     public void deleteCategory(@PathVariable int id)
     {
         // delete the category by id
+        categoryDao.delete(id);
     }
 }
--- capstone-starter/src/main/java/org/yearup/data/mysql/MySqlCategoryDao.java
+++ capstone-starter/src/main/java/org/yearup/data/mysql/MySqlCategoryDao.java
@@ -5,8 +5,8 @@ import org.yearup.data.CategoryDao;
 import org.yearup.models.Category;
 
 import javax.sql.DataSource;
-import java.sql.ResultSet;
-import java.sql.SQLException;
+import java.sql.*;
+import java.util.ArrayList;
 import java.util.List;
 
 @Component
@@ -21,21 +21,72 @@ public class MySqlCategoryDao extends MySqlDaoBase implements CategoryDao
     public List<Category> getAllCategories()
     {
         // get all categories
-        return null;
+        List<Category> categories = new ArrayList<>();
+        try {
+            Connection connection = getConnection();
+            String query = """
+                    SELECT *
+                    FROM categories;""";
+            PreparedStatement statement = connection.prepareStatement(query);
+            ResultSet results = statement.executeQuery();
+            while (results.next()) {
+                int id = results.getInt("category_id");
+                String name = results.getString("name");
+                String description = results.getString("description");
+                categories.add(new Category(id, name, description));
+            }
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+        return categories;
     }
 
     @Override
     public Category getById(int categoryId)
     {
         // get category by id
-        return null;
+        Category category;
+        try {
+            Connection connection = getConnection();
+            String query = """
+                    SELECT *
+                    FROM categories
+                    WHERE category_id = ?;""";
+            PreparedStatement statement = connection.prepareStatement(query);
+            statement.setInt(1, categoryId);
+            ResultSet results = statement.executeQuery();
+            results.next();
+            int id = results.getInt("category_id");
+            String name = results.getString("name");
+            String description = results.getString("description");
+            category = new Category(id, name, description);
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+        return category;
     }
 
     @Override
     public Category create(Category category)
     {
         // create a new category
-        return null;
+        try {
+            Connection connection = getConnection();
+            String create = """
+                    INSERT INTO categories (name, description)
+                    VALUES (?, ?);""";
+            PreparedStatement statement = connection.prepareStatement(create, Statement.RETURN_GENERATED_KEYS);
+            statement.setString(1, category.getName());
+            statement.setString(2, category.getDescription());
+            statement.executeUpdate();
+            ResultSet keys = statement.getGeneratedKeys();
+            keys.next();
+            int primaryKey = keys.getInt(1);
+            category.setCategoryId(primaryKey);
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+        return category;
     }
 
     @Override
