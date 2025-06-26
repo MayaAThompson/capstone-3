0201f4646e18d4adfbc76292f8e7a06b9981d9f2
Maya Thompson <mayaandromedathompson@gmail.com>
2025-06-26 15:15:54 -0500

    unfinished endpoint now throws 501 not implemented

--- capstone-starter/src/main/java/org/yearup/controllers/OrdersController.java
+++ capstone-starter/src/main/java/org/yearup/controllers/OrdersController.java
@@ -1,11 +1,13 @@
 package org.yearup.controllers;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.security.access.annotation.Secured;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.server.ResponseStatusException;
 import org.yearup.data.OrderDao;
 import org.yearup.data.ShoppingCartDao;
 import org.yearup.data.UserDao;
@@ -32,7 +34,7 @@ public class OrdersController {
 
     @PostMapping("")
     public void postOrder(Principal principal) {
-
+        throw new ResponseStatusException(HttpStatus.NOT_IMPLEMENTED);
     }
 
     public OrderDao getOrderDao() {
